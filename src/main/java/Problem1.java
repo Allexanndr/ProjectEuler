b79052326111a786cc3c46bb49e5cba580078b48
alexandrchshankin <aleksandrshchankin@ya.ru>
2023-01-05 02:04:53 +0300

    Initial commit

---
+++ src/main/java/Problem1.java
@@ -0,0 +1,14 @@
+
+
+public class Problem1 {
+    public static void main(String[] args) {
+        int result = 0;
+        for(int i = 0; i < 1000; i++ ){
+            if (i % 3 == 0 || i % 5 == 0) {
+               result = result + i;
+            }
+
+        }
+        System.out.println(result);
+    }
+}
